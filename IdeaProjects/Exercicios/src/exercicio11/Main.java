e1041490d1877f9bfdbda4dd6d62c907e939bb6b
Willzera <113561581+Wssfilho@users.noreply.github.com>
2023-09-20 15:05:20 -0300

    windows connect

--- IdeaProjects/Exercicios/src/exercicio11/Main.java
+++ IdeaProjects/Exercicios/src/exercicio11/Main.java
@@ -5,7 +5,6 @@ import main.lampada;
 public class Main {
     public static void main(String[] args)
     {
-
         Candidato c1, c2, c3;
         c1 = new Candidato(30.0, 20.0, 1.0, "wilson");
         c2 = new Candidato(51.0, 42.0, 53.0, "Alice");
