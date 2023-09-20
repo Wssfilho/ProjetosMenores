5e5947f194f8e090e74bc200d70c21d6d22d85f6
Willzera <113561581+Wssfilho@users.noreply.github.com>
2023-09-20 19:17:50 -0300

    windows connect

---
+++ IdeaProjects/Exercicios/src/exercicio12/ItemCompra.java
@@ -0,0 +1,41 @@
+package exercicio12;
+
+public class ItemCompra {
+    private String itemdec;
+    private float valor;
+    private int qtd;
+    private float perDesconto;
+
+    ItemCompra(String desc, int pqtd, float value)
+    {
+        this.itemdec = desc;
+        this.qtd = pqtd;
+        this.valor = value;
+
+    }
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    public int getQtd() {
+        return qtd;
+    }
+
+    public void setQtd(int qtd) {
+        this.qtd = qtd;
+    }
+
+    public String getItemdec() {
+        return itemdec;
+    }
+
+    public void setItemdec(String itemdec) {
+        this.itemdec = itemdec;
+    }
+
+
+}
