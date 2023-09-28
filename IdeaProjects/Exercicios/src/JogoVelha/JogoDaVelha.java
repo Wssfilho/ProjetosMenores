5627e5c4fb18fadd6f2620bf26622a3f9136911f
Willzera <113561581+Wssfilho@users.noreply.github.com>
2023-09-28 13:25:38 -0300

    add

--- IdeaProjects/Exercicios/src/JogoVelha/JogoDaVelha.java
+++ IdeaProjects/Exercicios/src/JogoVelha/JogoDaVelha.java
@@ -2,5 +2,27 @@ package JogoVelha;
 
 public class JogoDaVelha
 {
+    private char[][] tab;
+    private int limDir, limInf;
+    private int opX, opO;
+
+    JogoDaVelha()
+    {
+        this.limDir = 3;
+        this.limInf = 3;
+        tab = new char[this.limDir][this.limInf];
+        int i, j;
+        for(i = 0; i <= 3; i++)
+        {
+            for(j = 0; j <= 3; j++)
+            {
+                this.tab[i][j] = '.';
+            }
+        }
+    }
+
+
+
+
 
 }
