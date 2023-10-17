b66f1cdb6a9d564034034488c1ad9d2b5af5c571
Willzera <113561581+Wssfilho@users.noreply.github.com>
2023-10-16 22:19:44 -0300

    add

---
+++ ConsoleApp1/ConsoleApp1/Sensor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+     public class Sensor
+    {
+        private double iD;
+        private bool status;
+        public Sensor(double id)
+        {
+            this.iD = id;
+            this.status = false;
+        }
+        public bool IsStatus { get { return status; }
+            set { status = value; }
+        }
+        public double ID
+        {
+            get { return iD; }
+            set { iD = value; }
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine("ID: " + this.iD);
+            Console.WriteLine("Status " + IsStatus);
+        }
+    }
+}
