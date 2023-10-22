496be549821d81f303e4f989faaf652bd9c385d6
Willzera <113561581+Wssfilho@users.noreply.github.com>
2023-10-21 22:11:10 -0300

    modified:   index.html 	modified:   script.js 	modified:   styles.css

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
-class Agricultura 
-{
+class Agricultura {
     constructor(plantacao, tecnologia) {
         this.plantacao = plantacao;
         this.tecnologia = tecnologia;
     }
-    
-    informacao() 
-    {
+
+    informacao() {
         return `A plantação de ${this.plantacao} está utilizando a tecnologia ${this.tecnologia}.`;
     }
 }
-function criar()
-{
+
+var agriculturas = []; // array para armazenar os objetos
+
+function criar() {
     var plantacao = document.querySelector("#plantacao").value;
     var tecnologia = document.querySelector("#tecnologia").value;
-if (plantacao == "" || tecnologia == "") {
-    alert("Preencha todos os campos!");
-    return;
-}
-Agricultura = new Agricultura(plantacao, tecnologia);
+    if (plantacao == "" || tecnologia == "") {
+        alert("Preencha todos os campos!");
+        return;
+    }
+    var novaAgricultura = new Agricultura(plantacao, tecnologia);
+    agriculturas.push(novaAgricultura); // adiciona o novo objeto ao array
 }
 
-function status() 
-{
+function status() {
+    var Agricultura = agriculturas[agriculturas.length - 1]; // pega o último objeto adicionado
     if (Agricultura.tecnologia > 0 && Agricultura.tecnologia <= 3) {
-        document.querySelector("[name='txtstart']").value = "A plantação está em fase inicial.";
+        document.querySelector("[name='txtstart']").value = "Fase inicial.";
     }
     else if (Agricultura.tecnologia > 3 && Agricultura.tecnologia <= 6) {
-        document.querySelector("[name='txtstart']").value = "A plantação está em fase de crescimento.";
+        document.querySelector("[name='txtstart']").value = "Fase de crescimento.";
     }
     else if (Agricultura.tecnologia > 6 && Agricultura.tecnologia <= 9) {
-        document.querySelector("[name='txtstart']").value = "A plantação está em fase de maturação.";
+        document.querySelector("[name='txtstart']").value = "Fase de maturação.";
     }
     else if (Agricultura.tecnologia > 9 && Agricultura.tecnologia <= 12) {
-        document.querySelector("[name='txtstart']").value = "A plantação está em fase de colheita.";
+        document.querySelector("[name='txtstart']").value = "Fase de colheita.";
     }
-    else 
-    {
-        document.querySelector("[name='txtstart']").value = "A plantação está em fase de decomposição.";
+    else {
+        document.querySelector("txtstart").value = "A plantação está em fase de decomposição.";
     }
 }
