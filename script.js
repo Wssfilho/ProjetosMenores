a57b9bd3de03ff4983153af845f222399f3246cc
Willzera <113561581+Wssfilho@users.noreply.github.com>
2023-10-23 11:31:27 -0300

    modified:   index.html 	modified:   script.js 	modified:   styles.css

--- script.js
+++ script.js
@@ -5,10 +5,39 @@ class Agricultura {
     }
 
     informacao() {
-        return `A plantação de ${this.plantacao} está utilizando a tecnologia ${this.tecnologia}.`;
+        return `Usando: Tecnologia ${this.tecnologia}`;
+    }
+    get plantacao() {
+        return this._plantacao;
+    }
+    set plantacao(value) {
+        this._plantacao = value;
+    }
+    get tecnologia() {
+        return this._tecnologia;
+    }
+    set tecnologia(value) {
+        this._tecnologia = value;
     }
 }
+class Motor extends Agricultura {
+    constructor(plantacao, tecnologia, tipo) {
+        super(plantacao, tecnologia);
+        this.tipo = tipo;
+    }
 
+    informacao() {
+        return `Usando: ${this.tipo}`;
+    }
+
+    get tipo() {
+        return this._tipo;
+    }
+
+    set tipo(value) {
+        this._tipo = value;
+    }
+}
 var agriculturas = []; // array para armazenar os objetos
 
 function criar() {
@@ -23,9 +52,12 @@ function criar() {
 }
 
 function status() {
+    Agricultura = new Motor()
     var Agricultura = agriculturas[agriculturas.length - 1]; // pega o último objeto adicionado
     if (Agricultura.tecnologia > 0 && Agricultura.tecnologia <= 3) {
         document.querySelector("[name='txtstart']").value = "Fase inicial.";
+        var motor = new Motor(Agricultura.plantacao, Agricultura.tecnologia, "Trator");
+        document.querySelector("[name='tipo']").value = motor.informacao()
     }
     else if (Agricultura.tecnologia > 3 && Agricultura.tecnologia <= 6) {
         document.querySelector("[name='txtstart']").value = "Fase de crescimento.";
@@ -40,3 +72,4 @@ function status() {
         document.querySelector("txtstart").value = "A plantação está em fase de decomposição.";
     }
 }
+
