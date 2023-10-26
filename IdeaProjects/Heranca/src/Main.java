67769c521ff1ba8f2001e9c2bb36bc0bb686c54c
Willzera <113561581+Wssfilho@users.noreply.github.com>
2023-10-26 20:39:50 -0300

    Atualizado

--- IdeaProjects/Heranca/src/Main.java
+++ IdeaProjects/Heranca/src/Main.java
@@ -4,7 +4,7 @@ public class Main
 {
     public static void main(String[] args) {
         Figura2D [] desenho = new Figura2D[4];
-        desenho[0] = new Figura2D();
+        desenho[0] = new Figura2D(); //com abstract some
         desenho[1] = new Circulo(2.0, 3.0, 4.0);
         desenho[2] = new Circulo(); //sobrecarga de Poli
         desenho[3] = new Retangulo(2.0, 3.0, 5.0, 7.0);
