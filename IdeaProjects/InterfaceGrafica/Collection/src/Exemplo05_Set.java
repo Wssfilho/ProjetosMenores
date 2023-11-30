539ddec0b473126894e47b1e87751c2d6162a305
Willzera <113561581+Wssfilho@users.noreply.github.com>
2023-11-30 09:25:20 -0300

    comments edit

--- IdeaProjects/InterfaceGrafica/Collection/src/Exemplo05_Set.java
+++ IdeaProjects/InterfaceGrafica/Collection/src/Exemplo05_Set.java
@@ -1,17 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Exemplos;
 
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
-
-/**
- *
- * @author Admin
- */
 public class Exemplo05_Set {
 
    public static void main(String args[])
@@ -19,15 +9,15 @@ public class Exemplo05_Set {
         // Variáveis e seus tipos
         Set<String> eset = new HashSet<String>();
         // Principais operações        
-        eset.add("Fabiana");
-	eset.add("Carlos");
-	eset.add("Cristina");
-	eset.add("Rita");
-	eset.add("Flávia");
-	eset.add("José");        
-        // Verificando as duplicatas
-        eset.add("Carlos");
-        eset.add("Rita");
+       eset.add("Flávia");
+       eset.add("Cristina");
+       eset.add("Fabiana");
+       eset.add("Carlos");
+       eset.add("Rita");
+       eset.add("José");
+       // Verificando as duplicatas
+       eset.add("Rita");
+       eset.add("Carlos");
 
         System.out.println("--");                
         System.out.println("-- Resultado 1 --");
