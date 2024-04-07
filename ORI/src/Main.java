d71f6cac89bdc7b2a8a35859b23576a9dae7b285
Willzera <113561581+Wssfilho@users.noreply.github.com>
2024-04-07 15:26:40 -0300

    add functions

--- ORI/src/Main.java
+++ ORI/src/Main.java
@@ -2,89 +2,176 @@ import java.io.*;
 import java.awt.*;
 import java.nio.file.*;
 import javax.swing.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Main extends JFrame {
-
-
     // declaração dos botoes
-    JButton btnCriarDir, btnCriarArq, btnRenomear, btnCopiar, enviar;
-    JButton btnnovo;
-    JPanel formularioPane;
-    JButton botao;
+    JButton btnCriarDir, btnCriarArq, btnRenomear, btnCopiar;
+    JButton btnarquivos, btnarquivos2, mostrar, verificar, criar;
     JTextField texto;
-    private static final JFrame f = new Main();// Criando uma instância de JFrame
-    private static JFrame janela2;
+    private static final JFrame janela2 = new Main();// Criando uma instância de JFrame
 
-    Main() {
-//        /*Isso será um novo método, pois terá um programa de início na qual irá chamar os outros programas*/
+    Main()
+    {
+        mostrar = new JButton("Mostrar arquivo");
+        btnarquivos = new JButton("Manipular arquivos");
+        btnarquivos2 = new JButton("Escrita & Leitura");
+        JPanel janelaJPanel = new JPanel();
+        janelaJPanel.setLayout(new GridLayout(1, 2));
+        janelaJPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        janelaJPanel.add(btnarquivos);
+        janelaJPanel.add(btnarquivos2);
+        btnarquivos.addActionListener(e->novoframe());
+        btnarquivos2.addActionListener(e->janela3());
+        //mostrar.addActionListener(e->mostrarCSV());
+
+        Container contentPane = getContentPane();// Obtém o painel de conteúdo da janela principal
+        contentPane.add(janelaJPanel, BorderLayout.NORTH);
+    }
+
+    private void janela3()
+    {
+        mostrar = new JButton("Mostrar arquivos");
+        criar = new JButton("Criar CSV");
+        verificar = new JButton("Verificar");
+        JFrame manipulacao = new JFrame();
+        manipulacao.setBounds(0, 0, 400, 400);
+        manipulacao.setVisible(true);
+        manipulacao.setTitle("Primeira Janela");
+        manipulacao.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel formularioPane3 = new JPanel();
+        formularioPane3.setLayout(new GridLayout(4, 2));// define o layout do painel como uma grade com 4 linhas e 2
+        // colunas.
+        formularioPane3.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10)); // Aqui, estamos definindo uma borda
+        formularioPane3.add(mostrar);
+        formularioPane3.add(criar);
+        formularioPane3.add(verificar);
+        mostrar.addActionListener(e->mostrarCSV());
+        criar.addActionListener(e->io());
+        verificar.addActionListener(e->status());
+        Container contentPane3 = manipulacao.getContentPane();// Obtém o painel de conteúdo da janela principal
+        contentPane3.add(formularioPane3, BorderLayout.NORTH);
+
+    }
+    public void status() {
+        File arquivo = new File("C:\\adriele", "dados.csv");
+        StringBuilder sb = new StringBuilder();
+        sb.append("Tamanho do arquivo: ").append(length(arquivo)).append(" bytes\n");
+        sb.append("Última modificação: ").append(new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new Date(lastModified(arquivo)))).append("\n");
+        sb.append("Caminho do arquivo: ").append(toString(arquivo)).append("\n");
+        sb.append("Status de delete: ").append((delete(arquivo)));
+        JOptionPane.showMessageDialog(null, sb.toString());
+    }
+
+    // Método para obter o tamanho do arquivo
+    public long length(File arquivo) {
+        return arquivo.length();
+    }
+
+    // Método para obter a data da última modificação do arquivo
+    public long lastModified(File arquivo) {
+        return arquivo.lastModified();
+    }
+
+    // Método para obter o caminho do arquivo como uma string
+    public String toString(File arquivo) {
+        return arquivo.getPath();
+    }
+    public boolean delete(File arquivo) {
+        return arquivo.delete();
+    }
+
+    public void mostrarCSV() {
+        String csvFile = "C:\\adriele\\dados.csv";
+        String line = "";
+        String csvSplitBy = ",";
+        StringBuilder sb = new StringBuilder();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+            br.readLine(); // lê e descarta a primeira linha
+            sb.append(String.format("%-10s %-10s %-10s%n", "Nome:", "Idade:", "Cidade:"));
+            while ((line = br.readLine()) != null) {
+                String[] campos = line.split(csvSplitBy);
+                sb.append(String.format("%-10s %-10s %-10s%n", campos[0], campos[1], campos[2]));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        JOptionPane.showMessageDialog(null, sb.toString());
+
+    }
+
+    private void io() {
+        // Cria um objeto File para o arquivo CSV
+        File arquivoCSV = new File("C:\\adriele\\dados.csv");
+        if(arquivoCSV.exists())
+        {
+        JOptionPane.showMessageDialog(null, "Já existe o arquivo");
+            return;
+        }
+        // Cria um PrintWriter para escrever no arquivo
+        PrintWriter escritor = null;
+        try {
+            escritor = new PrintWriter(new FileOutputStream(arquivoCSV));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Escreve a cabeçalho do CSV
+        escritor.println("Nome,Idade,Cidade");
+
+        // Obtém os dados do usuário usando JOptionPane
+        for (int i = 0; i < 5; i++) {
+            String nome = JOptionPane.showInputDialog("Digite seu nome:");
+            int idade = Integer.parseInt(JOptionPane.showInputDialog("Digite sua idade:"));
+            String cidade = JOptionPane.showInputDialog("Digite sua cidade:");
+
+            // Escreve os dados no arquivo CSV
+            escritor.println(nome + "," + idade + "," + cidade);
+        }
+
+        // Fecha o PrintWriter
+        escritor.close();
+
+        // Exibe uma mensagem de sucesso
+        JOptionPane.showMessageDialog(null, "Dados gravados com sucesso no arquivo " + arquivoCSV.getPath());
+    }
+    private void novoframe() {
+        /*Isso será um novo método, pois terá um programa de início na qual irá chamar os outros programas*/
         btnCriarDir = new JButton("CriarDiretorio");// Cria um botão com o texto "Criar Diretório"
         btnCriarArq = new JButton("CriarArquivo");// Cria um botão com o texto "Criar Arquivo"
         btnRenomear = new JButton("RenomearArquivo");// Cria um botão com o texto "Renomear Arquivo"
         btnCopiar = new JButton("Copiar Arquivo");// Cria um botão com o texto "Copiar Arquivo"
-        btnnovo = new JButton("Nova janela");
-
-        // Um JPanel é um componente de contêiner leve que pode ser usado para agrupar
-        // outros componentes em uma interface gráfica.
+        JFrame f = new JFrame();
+        f.setBounds(0, 0, 400, 400);
+        f.setVisible(true);
+        f.setTitle("Primeira Janela");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JPanel formularioPane = new JPanel();
         formularioPane.setLayout(new GridLayout(4, 2));// define o layout do painel como uma grade com 4 linhas e 2
-                                                       // colunas.
+        // colunas.
         formularioPane.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10)); // Aqui, estamos definindo uma borda
-                                                                                   // vazia (sem margens) para o painel.
+        // vazia (sem margens) para o painel.
         // Adicionando os controles no formulario. Ou seja na tela
         formularioPane.add(btnCriarDir);
         formularioPane.add(btnCriarArq);
         formularioPane.add(btnRenomear);
         formularioPane.add(btnCopiar);
-        formularioPane.add(btnnovo);
-        btnnovo.addActionListener(e -> novoframe());
         btnCopiar.addActionListener(e -> copiarArquivo());
         btnCriarDir.addActionListener(e -> criarDiretorio());// Configura um ouvinte de ação para o botão "Criar
-                                                             // Diretório"
+        // Diretório"
         btnCriarArq.addActionListener(e -> criarArquivo());// Configura um ouvinte de ação para o botão "Criar Arquivo"
         btnRenomear.addActionListener(e -> renomearArquivo());// Configura um ouvinte de ação para o botão "Renomear"
-        Container contentPane = getContentPane();// Obtém o painel de conteúdo da janela principal
-        contentPane.add(formularioPane, BorderLayout.NORTH);// Adiciona o painel do formulário ao painel de conteúdo,
-                                                            // posicionando-o na parte superior (Norte)
+        Container contentPane2 = f.getContentPane();// Obtém o painel de conteúdo da janela principal
+        contentPane2.add(formularioPane, BorderLayout.NORTH);
+        // posicionando-o na parte superior (Norte)
+        //janela2.dispose();
 
-    }
-
-    
-    public void novoframe() {
-        botao = new JButton("Novo botao");
-        enviar = new JButton("Enviar");
-        janela2 = new JFrame();
-        janela2.setBounds(0, 0, 400, 400);
-        janela2.setVisible(true);
-        janela2.setTitle("Primeira Janela");
-        janela2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        texto = new JTextField();
-        JPanel janelaJPanel = new JPanel();
-        janelaJPanel.setLayout(new GridLayout(1, 2));
-        janelaJPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-        janelaJPanel.add(botao);
-        janelaJPanel.add(enviar);
-        janelaJPanel.add(texto);
-        f.dispose();
-
-        enviar.addActionListener(e -> enviar());
-        Container contentPane2 = janela2.getContentPane(); // janela dois porque é segunda janela
-        contentPane2.add(janelaJPanel, BorderLayout.NORTH);
     }
     //retirar
-    public void enviar() 
-    {
-        try {
-            String a = texto.getText();
-            texto.setText("");
-            if (a.isEmpty()) {
-                JOptionPane.showMessageDialog(null, "CAMPO vazio");
-            } else {
-                JOptionPane.showMessageDialog(rootPane, a);
-            }       
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
 
-    }
 
     public void copiarArquivo() {
         // Define o caminho do arquivo original
@@ -209,10 +296,10 @@ public class Main extends JFrame {
 
     public static void main(String[] args) {
 
-        f.setTitle("Primeira Janela");// Definindo o título da janela
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Configurando a ação padrão ao fechar a janela
-        f.setBounds(300, 300, 400, 400);// Definindo as dimensões e posição da janela
-        f.setVisible(true);// Tornando a janela visível
+        janela2.setTitle("Menu");// Definindo o título da janela
+        janela2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Configurando a ação padrão ao fechar a janela
+        janela2.setBounds(300, 300, 400, 100);// Definindo as dimensões e posição da janela
+        janela2.setVisible(true);// Tornando a janela visível
     }
 
 }
