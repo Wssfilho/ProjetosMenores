d48ddad0bf849cb7706083abc1934d6783e6b377
Willzera <113561581+Wssfilho@users.noreply.github.com>
2024-04-29 09:17:23 -0300

    adicionado a funcao de busca e tratamentos de excessões.

--- Organização e Recuperação da Info/lsencadeada/src/Principal.java
+++ Organização e Recuperação da Info/lsencadeada/src/Principal.java
@@ -15,7 +15,8 @@ class No {
 
 class ListaLigada {
     No cabeca;
-    void insereNoInicio(No novoNo) {
+
+    public void insereNoInicio(No novoNo) {
         if (cabeca != null) {
             novoNo.proximo = cabeca;
             cabeca.anterior = novoNo;
@@ -23,7 +24,7 @@ class ListaLigada {
         cabeca = novoNo;
     }
 
-    void insereNoFim(No novoNo) {
+    public void insereNoFim(No novoNo) {
         if (cabeca == null) {
             cabeca = novoNo;
         } else {
@@ -36,62 +37,88 @@ class ListaLigada {
         }
     }
 
-    void exibe(JTextArea areaTexto) {
+    public void exibe(JTextArea areaTexto) {
         No temp = cabeca;
         while (temp != null) {
             areaTexto.append(temp.valor + " ");
             temp = temp.proximo;
         }
     }
+
+    public int buscarNo(int valor) {
+        No alvo = cabeca;
+        while (alvo != null && alvo.valor != valor) {
+            alvo = alvo.proximo;
+        }
+
+        return alvo.valor;
+    }
 }
 
 public class Principal {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                JFrame quadro = new JFrame();
-                quadro.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                quadro.setSize(400, 300);
-                quadro.setLayout(new GridLayout(10, 2)); // Define o layout do painel como uma grade com o número de linhas igual ao número de botões e 2 colunas.
-
-                ListaLigada lista = new ListaLigada();
-                JTextField campoTexto = new JTextField(10);
-                quadro.add(campoTexto);
-                JTextArea areaTexto = new JTextArea(2, 10);
-                JScrollPane painelRolagem = new JScrollPane(areaTexto);
-                quadro.add(painelRolagem);
-                JButton botaoInicio = new JButton("Inserir no Início");
-
-                botaoInicio.addActionListener(e -> {
-                    if(campoTexto.getText().isEmpty())
-                    {
-                       JOptionPane.showMessageDialog(null, "Campo vazio");
-                    }
-                    else {
-                        int valor = Integer.parseInt(campoTexto.getText());
-                        No novoNo = new No(valor);
-                        lista.insereNoInicio(novoNo);
-                        areaTexto.setText("");
-                        campoTexto.setText("");
-                        lista.exibe(areaTexto);
-                    }
+        JButton botaoInicio, botaoFim, botaobusca;
+        JTextField campoTexto, campoBusca;
+        JTextArea areaTexto;
+        JScrollPane painelRolagem;
+        JFrame quadro = new JFrame();
+        quadro.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        quadro.setSize(400, 300);
+        quadro.setLayout(new GridLayout(10, 2)); // Define o layout do painel como uma grade com o número de linhas igual ao número de botões e 2 colunas.
+        botaoInicio = new JButton("Inserir no Início");
+        botaoFim = new JButton("Inserir no Fim");
+        ListaLigada lista = new ListaLigada();
+        campoTexto = new JTextField(10);
+        campoBusca = new JTextField(10);
+        quadro.add(campoTexto);
+        quadro.add(botaoInicio);
+        quadro.add(botaoFim);
+        areaTexto = new JTextArea(2, 10);
+        painelRolagem = new JScrollPane(areaTexto);
+        quadro.add(painelRolagem);
 
 
-                });
-                quadro.add(botaoInicio);
-
-                JButton botaoFim = new JButton("Inserir no Fim");
-                botaoFim.addActionListener(e -> {
-                    int valor = Integer.parseInt(campoTexto.getText());
-                    No novoNo = new No(valor);
-                    lista.insereNoFim(novoNo);
-                    areaTexto.setText("");
-                    campoTexto.setText("");
-                    lista.exibe(areaTexto);
-                });
-                quadro.add(botaoFim);
-                quadro.setVisible(true);
+        botaoInicio.addActionListener(e -> {
+            if (campoTexto.getText().isEmpty()) {
+                JOptionPane.showMessageDialog(null, "Campo vazio");
+            } else {
+                int valor = Integer.parseInt(campoTexto.getText());
+                No novoNo = new No(valor);
+                lista.insereNoInicio(novoNo);
+                areaTexto.setText("");
+                campoTexto.setText("");
+                lista.exibe(areaTexto);
             }
+
+
         });
+        botaoFim.addActionListener(e -> {
+            int valor = Integer.parseInt(campoTexto.getText());
+            No novoNo = new No(valor);
+            lista.insereNoFim(novoNo);
+            areaTexto.setText("");
+            campoTexto.setText("");
+            lista.exibe(areaTexto);
+        });
+        quadro.setVisible(true);
+        botaobusca = new JButton("Buscar elemento");
+        botaobusca.addActionListener(e ->
+        {
+            try {
+                if (campoBusca.getText().isEmpty()) {
+                    JOptionPane.showMessageDialog(null, "Campo vazio");
+                    return;
+                }
+                int valor = Integer.parseInt(campoBusca.getText());
+                JOptionPane.showMessageDialog(null, "Valor: " + lista.buscarNo(valor));
+            } catch (NullPointerException E) {
+                JOptionPane.showMessageDialog(null, "Valor não encontrado");
+            }
+
+        });
+        quadro.add(campoBusca);
+        quadro.add(botaobusca);
+
     }
+
 }
