2589e671c43fc25612591355cfce781532f49ef0
Willzera <113561581+Wssfilho@users.noreply.github.com>
2024-06-24 20:52:20 -0300

    feat: Adicionar função para verificar se um subtabuleiro 2x2 tem todas as cores diferentes

--- Organização e Recuperação da Info/wilson_adrielle_ori24/src/app2.java
+++ Organização e Recuperação da Info/wilson_adrielle_ori24/src/app2.java
@@ -1,20 +1,23 @@
 import java.sql.*;
+import java.util.concurrent.ExecutionException;
 
-import javax.naming.spi.DirStateFactory.Result;
+//import javax.naming.spi.DirStateFactory.Result;
 import javax.swing.*;
 import java.awt.*;
 
 public class app2 extends JFrame {
-    private JTextArea max, min, textArea3, textArea4, textArea5;
-    private JRadioButton buttonOrder, buttonGroup, buttonSum, buttonCount, buttonMax, buttonMin;
+    private JTextArea max, min, textArea3;
+    private JRadioButton buttonOrder, buttonGroup, buttonSum, buttonCount, buttonMax, buttonMin, buttonListCli, buttonListMed;
     JPanel panelRadio, panelEsquerdo, panelDireito;
     JTable table;
     public app2() {
-        setSize(800, 600);
-        setTitle("Exemplo Java - SQLite - VSCode");
+        setSize(900, 600);
+        setTitle("Consulta de Farmacia");
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new BoxLayout(getContentPane(), BoxLayout.X_AXIS));
+        setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+        buttonListCli = new JRadioButton("Listar todos clientes");
+        buttonListMed = new JRadioButton("Listar todos medicamentos");
         buttonOrder = new JRadioButton("Ordenar");
         buttonGroup = new JRadioButton("Agrupar");
         buttonSum = new JRadioButton("Somar");
@@ -27,11 +30,13 @@ public class app2 extends JFrame {
         min.setEditable(false);
 
         panelEsquerdo = new JPanel();
-        panelEsquerdo.setLayout(new BoxLayout(panelEsquerdo, BoxLayout.PAGE_AXIS));
-        textArea3 = new JTextArea(20,25);
+        panelEsquerdo.setLayout(new BoxLayout(panelEsquerdo, BoxLayout.X_AXIS));
+        textArea3 = new JTextArea(30, 25);
         panelEsquerdo.add(new JScrollPane(textArea3));
         panelRadio = new JPanel();
-        panelRadio.setLayout(new FlowLayout( FlowLayout.LEFT, 10, 10));
+        panelRadio.setLayout(new FlowLayout( FlowLayout.LEFT, 1, 10));
+        panelRadio.add(buttonListCli);
+        panelRadio.add(buttonListMed);
         panelRadio.add(buttonOrder);
         panelRadio.add(buttonGroup);
         panelRadio.add(buttonSum);
@@ -45,15 +50,33 @@ public class app2 extends JFrame {
         contentPane.add(panelRadio, BorderLayout.NORTH);
         contentPane.add(panelEsquerdo, BorderLayout.SOUTH);
         
-        // add(panelEsquerdo);
-        // add(panelDireito);
-
+       
         setVisible(true);
+        setResizable(false);
+        buttonListMed.addActionListener(e -> seeallmed());
         buttonMax.addActionListener(e -> infomax());
         buttonMin.addActionListener(e -> infomin());
         buttonGroup.addActionListener(e -> groupby());
     }
-
+    private void seeallmed()
+    {
+        try
+        {
+            Connection c = DriverManager.getConnection("jdbc:sqlite:BD_Farmacia.db");
+            Statement stm = c.createStatement();
+            ResultSet rs = stm.executeQuery("SELECT * FROM medicamentos");
+            StringBuilder sb = new StringBuilder();
+            while (rs.next()) {
+                sb.append("  - Nome Medicamento: ").append(rs.getString("Nome_med")).append("\n")
+                  .append("  - Tipo: ").append(rs.getString("tipo")).append("\n")
+                  .append("  - cod_fk: ").append(rs.getString("cod_fk")).append("\n");
+            }
+        textArea3.setText(sb.toString());
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
     private void infomax() {
         try {
             Connection c = DriverManager.getConnection("jdbc:sqlite:BD_Farmacia.db");
@@ -84,12 +107,14 @@ public class app2 extends JFrame {
     }
     private void groupby() {
         try {
+           // textArea3.setText(" ");
             Connection c = DriverManager.getConnection("jdbc:sqlite:BD_Farmacia.db");
             Statement stm = c.createStatement();
     
             ResultSet rs = stm.executeQuery("SELECT * FROM Cliente, Medicamentos WHERE Cliente.id = Medicamentos.cod_fk GROUP BY Cliente.nome");
             StringBuilder sb = new StringBuilder();
             String previousCustomerName = ""; // Store previous customer name
+            
     
             while (rs.next()) {
                 String currentCustomerName = rs.getString("Nome");
